bf6924bbb252d5b04301340d20e213d5d9565889
Hemendra Rajput <flp.hemendra123@gmail.com>
2023-04-30 23:41:56 +0530

    update project and add Cucumber-html-report

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -8,7 +8,9 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(features="src/test/java/features",
 plugin ={"pretty","json:target/jsonReports/cucumber-report.json",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-		,"rerun:target/failed_scenarios.txt"},
+		,"rerun:target/failed_scenarios.txt",
+		"html:target/HtmlReports/report.html",
+		},
 glue= {"stepDefinations"},tags= "")
 public class TestRunner {
 //tags= {"@DeletePlace"}  compile test verify
